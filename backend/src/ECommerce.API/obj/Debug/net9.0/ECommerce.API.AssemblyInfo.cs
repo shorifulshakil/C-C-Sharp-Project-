62f25fc966f85616bf15a397b815958b537b66d2
Md. Shoriful Islam Shakil <23303006@iubat.edu>
2026-09-16 23:55:57 +0600

    Update project paths and package references for ECommerce.Domain and ECommerce.Infrastructure
    - Changed user directory references from 'md.prantoislam' to 'shoriful_shakil' in project files.
    - Updated NuGet package paths and versions in project.assets.json and project.nuget.cache.
    - Increased SDK analysis level from 9.0.300 to 10.0.400.
    - Updated assembly informational version in ECommerce.Infrastructure.
    - Adjusted SourceRoot and NuGetPackageRoot paths in .csproj.nuget.g.props files.
    - Added download dependencies for Microsoft.AspNetCore.App.Ref and Microsoft.NETCore.App.Ref.

--- backend/src/ECommerce.API/obj/Debug/net9.0/ECommerce.API.AssemblyInfo.cs
+++ backend/src/ECommerce.API/obj/Debug/net9.0/ECommerce.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391795e5fc7b940b2c12226f19c64cdfa25e8705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0182bd6426b02b603b3189be8ce269be6cdf5683")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
