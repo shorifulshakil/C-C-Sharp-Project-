334b95da797252a9878519409dc1fff9a815f920
Md. Shoriful Islam Shakil <23303006@iubat.edu>
2026-09-17 13:16:38 +0600

    Update assembly informational version and cache files for ECommerce components

--- backend/src/ECommerce.Infrastructure/obj/Debug/net9.0/ECommerce.Infrastructure.AssemblyInfo.cs
+++ backend/src/ECommerce.Infrastructure/obj/Debug/net9.0/ECommerce.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0182bd6426b02b603b3189be8ce269be6cdf5683")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cfc981b0b928b8c4b35b75f944fdbc7df92ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
